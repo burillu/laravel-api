203aea523d6e781a2d12866a1da9e4dc8d067602
burillu <burillu90@hotmail.it>
2024-01-24 16:37:50 +0100

    create newcontact class, and template to send emails

---
+++ resources/views/mails/new-contact-mail.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <h1>Boolfolio Ebau Michele</h1>
+    <p>Ciao Michele,<br>
+        Hai ricevuto un messaggio, ecco i dettagli: <br>
+        Nome: {{ $lead->name }} <br>
+        Email: {{ $lead->email }} <br>
+        Message: <br>
+        {{ $lead->message }}
+
+
+
+    </p>
+</div>
